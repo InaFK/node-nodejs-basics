f9232b093625c438f45e21c8d6808fb6a904e05e
Ina <ina.flaryanovich@gmail.com>
2024-10-07 20:15:52 +0200

    feat: implement print array of filenames function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, 'files');
+
+  try {
+    await fs.access(folderPath);
+    const files = await fs.readdir(folderPath);
+    console.log(files);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed: The "files" folder does not exist.');
+    } else {
+      throw new Error(`FS operation failed: ${error.message}`);
+    }
+  }
 };
 
 await list();
